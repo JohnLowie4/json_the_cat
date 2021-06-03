6442ea8a7544a08ad93b77b6f3baf118df4bb2ee
John <jzlowie@gmail.com>
2021-06-03 17:14:40 -0600

    refactored and exported for index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const request = require('request');
-const args = process.argv;
-const breed = args[2];
-const catBreed = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const catBreed = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request(catBreed, (error, response, body) => {
-  if (error) {
-    console.error("Request Failed");
-  } else if (!error) {
-    const data = JSON.parse(body)[0];
-    if (!data) console.log('Breed Not Found');
-    if (data) console.log(data.description);
-  }
-});
+const fectchBreedDescription = function (breedName, callback) {
+  request(catBreed + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else if (!error) {
+      const data = JSON.parse(body)[0];
+      if (!data) callback(null, "Breed Not Found");
+      if (data) callback(null, data.description);
+    }
+  });
+};
+
+module.exports = {
+  fectchBreedDescription
+};
